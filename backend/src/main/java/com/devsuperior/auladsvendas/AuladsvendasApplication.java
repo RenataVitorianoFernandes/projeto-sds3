04b99b3c90bc70910701d7382702e995a480b7fb
RenataVitorianoFernandes <revitoriano@hotmail.com>
2021-05-07 20:01:53 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/auladsvendas/AuladsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.auladsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuladsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuladsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/auladsvendas/AuladsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.auladsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuladsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
